df64bd4cc6701f70aec2e6a98789a6fc8c8c1504
jefflin-git <75293843+jefflin-git@users.noreply.github.com>
2026-02-18 15:47:21 +0800

    fix: Modify file_path name.

--- back-end/fastapi/repositories/geo.py
+++ back-end/fastapi/repositories/geo.py
@@ -1,20 +1,21 @@
 import pandas as pd
 import os
+from common import REPORT_TABLE_FILE_NAME
 
-file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".", "datas", "資料訓練大表_v1.1.csv"))
+report_table_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".", "datas", f"{REPORT_TABLE_FILE_NAME}.csv"))
 
 class GeoRepository:
     @staticmethod
     def get_city_list() -> list[str]:
-        data = pd.read_csv(file_path)
+        data = pd.read_csv(report_table_file_path)
         return data["縣市"].unique().tolist()
 
     @staticmethod
     def get_district_list(city: str) -> list[str]:
-        data = pd.read_csv(file_path)
+        data = pd.read_csv(report_table_file_path)
         return data[data["縣市"] == city]["行政區"].unique().tolist()
     
     @staticmethod
     def get_neighborhood_list(city: str, district: str) -> list[str]:
-        data = pd.read_csv(file_path)
+        data = pd.read_csv(report_table_file_path)
         return data[(data["縣市"] == city) & (data["行政區"] == district)]["里別"].unique().tolist()
