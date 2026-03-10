3d46cbe24ba8f910046a20e9720507a07d7941c3
jefflin-git <75293843+jefflin-git@users.noreply.github.com>
2026-03-10 20:00:09 +0800

    feat: Change back-end service url.

--- front-end/angular/src/common.ts
+++ front-end/angular/src/common.ts
@@ -1,5 +1,5 @@
-// export const BACK_END_URL = "https://ai-project-backend-438992102752.asia-east1.run.app/api";
-export const BACK_END_URL = "http://localhost:8000/api";
+export const BACK_END_URL = "https://ai-project-backend-438992102752.asia-east1.run.app/api";
+// export const BACK_END_URL = "http://localhost:8000/api";
 export const HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
 };
